d5d7ce6095891377f1163370eddfdc7dd04d4839
Krzykrzychu2020 <64839677+Krzykrzychu2020@users.noreply.github.com>
2020-07-04 00:29:56 +0200

    Add files via upload

---
+++ AppStore/Client.java
@@ -0,0 +1,10 @@
+public class Client {
+    public Personality personality;
+
+    public enum Personality{
+    }
+
+    public Client(Personality personality) {
+        this.personality = personality;
+    }
+}
